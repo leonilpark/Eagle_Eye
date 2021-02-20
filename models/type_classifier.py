3634dae7ea4fdb848c82768341877c0011e8a991
leonilpark <58671177+leonilpark@users.noreply.github.com>
2021-02-21 05:33:05 +0900

    Update type_classifier.py

--- models/type_classifier.py
+++ models/type_classifier.py
@@ -2,8 +2,21 @@ import torch
 
 
 class TypeClassifier(torch.nn.Module):
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, block_args, global_params, image_size=None):
+        super().__init__()
+        self._block_args = block_args
+        self._bn_mom = 1 - global_params.batch_norm_momentum # pytorch's difference from tensorflow
+        self._bn_eps = global_params.batch_norm_epsilon
+        self.has_se = (self._block_args.se_ratio is not None) and (0 < self._block_args.se_ratio <= 1)
+        self.id_skip = block_args.id_skip
+        def forward(self, inputs):
+        # Convolution layers
+        x = self.extract_features(inputs)
 
-    def forward(self, X):
-        raise NotImplementedError
+        # Pooling and final linear layer
+        x = self._avg_pooling(x)
+        x = x.flatten(start_dim=1)
+        x = self._dropout(x)
+        x = self._fc(x)
+
+        return x
